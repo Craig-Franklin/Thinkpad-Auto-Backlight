2e381f7e13c5c6401b8c5c706acb00af1a24acac
Craig Franklin <craig@csfranklin.com>
2020-06-17 16:21:17 +0100

    Initial checkin

---
+++ Thinkpad-Auto-Backlight-Service/Thinkpad-Auto-Backlight-Service/ProjectInstaller.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace Thinkpad_Auto_Backlight_Service
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : System.Configuration.Install.Installer
+    {
+        public ProjectInstaller()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Thinkpad-Auto-Backlight-Service/Thinkpad-Auto-Backlight-Service/Service1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thinkpad_Auto_Backlight_Service
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Keyboard_Core.KeyboardControl core = new Keyboard_Core.KeyboardControl();
+            //Set keyboard back light to brightest
+            core.SetKeyboardBackLightStatus(2);
+            //Stop the service
+            Stop();
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
